2f0f291a0b795015c6e4d4961be8ea87813197b7
Miranda Christensen <97591732+Mxxira@users.noreply.github.com>
2022-10-18 18:30:20 -0700

    Add files via upload

---
+++ public/home.js
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <Card
+      bgcolor="primary"
+      header="Home"
+      status=""
+      text="Welcome to Bad Bank!"
+      body="Here at BankBad, we offer the most reliable banking service with the most limited security. Here, we offer banking with ease and efficiency."
+    />
+  );
+}
